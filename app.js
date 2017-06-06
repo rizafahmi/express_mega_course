b1c3d4d65d4c8eb440006bd7862c8ffc7582121f
Riza Fahmi <rizafahmi@gmail.com>
2017-06-06 15:18:56 +0700

    Setup and using Pug

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  return res.send('Hello Express 🖖')
+  const user = {
+    username: 'riza'
+  }
+  return res.render('index', { user: user })
 })
 
 app.listen(3000, (err) => {
